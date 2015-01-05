42635ee757ac5d30f329a51688528c9c70d05421
ssx86 <zss2366@gmail.com>
2015-01-05 10:48:52 +0000

    count current word read time

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -12,5 +12,15 @@ class WordsController < ApplicationController
 
   def current_word
     @word = Word.find(params[:id])
+    @wr = current_user.user_words.find_by word_id:@word.id
+    if @wr then
+      #保护times为空的初始数据，release之后可以考虑移除
+      @wr.times ||= 1
+      @wr.times = @wr.times + 1 
+      @wr.save
+    else
+      puts "创建"
+      @wr = current_user.user_words.create(user_id:current_user.id, word_id:@word.id, times: 1)
+    end
   end
 end
--- app/models/user_word.rb
+++ app/models/user_word.rb
@@ -1,4 +1,6 @@
 class UserWord < ActiveRecord::Base
   belongs_to :user
   belongs_to :word
+
+  validates :word_id, uniqueness: { scope: :user_id }
 end
